1d1c03d9d521db85a54337f8d0c5a90d60509f16
Quyt <buiquangquyet@gmail.com>
2022-07-29 17:55:59 +0700

    demo kafka

--- app/Http/Controllers/ProducerController.php
+++ app/Http/Controllers/ProducerController.php
@@ -25,20 +25,19 @@ class ProducerController extends Controller
         $conf = new Conf();
         $conf->set('log_level', (string) LOG_DEBUG);
         $conf->set('debug', 'all');
-        $rk = new Producer($conf);
-        $rk->addBrokers("127.0.0.1:9092");
-
         $producer = new Producer($conf);
+        $producer->addBrokers("127.0.0.1:9092");
+
         $timeout_ms = 10000;
         $kafkaPurgeFlag = (int)env('KAFKA_PURGE_FLAG', 0);
-        $payload = ['ptest producer 1'];
+        $payload = 'hello kship toi co webhook ve';
         try {
             $topic = $producer->newTopic(env('KAFKA_QUEUE'));
-            $topic->produce(RD_KAFKA_PARTITION_UA, 0, json_encode($payload));
+            $topic->produce(RD_KAFKA_PARTITION_UA, env('KAFKA_PARTITION'), $payload);
             if ($kafkaPurgeFlag) {
                 $producer->purge(RD_KAFKA_PURGE_F_QUEUE);
             }
-            //$producer->flush($timeout_ms);
+            $producer->flush($timeout_ms);
             dd($producer);
         } catch (\Exception $e) {
             if ($kafkaPurgeFlag) {
